ed001a37f6e2e82f23bf868b50c3cb7ffc069ee3
Berkay Gürsoy <berkaygursoy@gmail.com>
2019-06-18 17:28:48 +0300

    refactor

--- public/controllers/Event/EventAttendController.js
+++ public/controllers/Event/EventAttendController.js
@@ -23,47 +23,12 @@ export default (app) => {
 
         EventCtrl.keys = keys;
 
-        EventCtrl.eventName = null;
-        EventCtrl.userName = null;
-        EventCtrl.startTime = null;
-        EventCtrl.city = null;
-        EventCtrl.state = null;
-        EventCtrl.zip = null;
-        EventCtrl.country = null;
-        EventCtrl.lat = null;
-        EventCtrl.long = null;
-        EventCtrl.description = null;
-
-        EventCtrl.checkStartTime = function () {
-            const currentTime = new Date();
-            if (EventCtrl.startTime < currentTime)
-                EventCtrl.startTime = currentTime;
-        }
-
-        EventCtrl.getResult = function () {
-            EventCtrl.rentRange = null;
+        EventCtrl.submit = function () {
             EventCtrl.loader = true;
             EventCtrl.disableButton = true;
 
-            EventCtrl.response = PredictService.getResult(EventCtrl.detail);
-            EventCtrl.response.then(function (response) {
-                EventCtrl.rentRange = response.data[0];
-                $window.scrollTo(0, 0);
-                EventCtrl.loader = false;
-                EventCtrl.disableButton = false;
-            }, function (response) {
-                console.log(response.status);
-                EventCtrl.loader = false;
-                EventCtrl.disableButton = false;
-            })
-        }
-
-        $('#flatpickr').flatpickr({
-            enableTime: true,
-            dateFormat: "Y-m-d H:i",
-            time_24hr: true,
-            minDate: "today"
-        });
-    }
-
-};
+            console.log(EventCtrl.attend);
+            console.log(EventCtrl.interest);
+        };
+    };
+}
