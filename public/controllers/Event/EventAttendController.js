e4036d5e7fb7ef6f15fbcc341a29aa3b8fbe36d0
Berkay Gürsoy <berkaygursoy@gmail.com>
2019-06-19 16:42:27 +0300

    event controller interaction

--- public/controllers/Event/EventAttendController.js
+++ public/controllers/Event/EventAttendController.js
@@ -1,34 +1,80 @@
 export default (app) => {
-
     app.controller('EventAttendController', EventAttendController);
 
     EventAttendController.$inject = ['$window'];
     function EventAttendController($window) {
         const EventCtrl = this;
 
-        const keys = {
-            "events": {
-                "SPA": 1361307272,
-                "HAVUZ": 955398943,
-                "KAYAK": 1600413013,
-                "BİNİCİLİK": 2877501688,
-                "GOLF": 2529072432,
-                "ATV TURU": 3583332424,
-                "TELEFERİK": 907302600,
-                "HAMAM": 507707719,
-                "LUNAPARK": 1927775201,
-                "BASKETBOL": 1532377761
-            }
+        const events = {
+            1361307272: "SPA",
+            955398943: "HAVUZ",
+            1600413013: "KAYAK",
+            2877501688: "BİNİCİLİK",
+            2529072432: "GOLF",
+            3583332424: "ATV",
+            907302600: "TELEFERİK",
+            507707719: "HAMAM",
+            1927775201: "LUNAPARK",
+            1532377761: "BASKETBOL"
         };
 
-        EventCtrl.keys = keys;
+        const users = {
+            151534919: "ALİ",
+            265212492: "VELİ",
+            2238882361: "CEMİL",
+            1776192: "AHMET",
+            23465780: "BÜLENT",
+            20018153: "TAHA",
+            15390083: "ŞAFAK",
+            7514340: "BERKAY",
+            50521271: "FATİH",
+            2034510440: "MAHMUT",
+            2664817652: "CANLI"
+        };
 
-        EventCtrl.submit = function () {
+        const attendStatusOptions = {
+            0: "No",
+            1: "Maybe",
+            2: "Yes"
+        }
+
+        EventCtrl.events = events;
+        EventCtrl.users = users;
+        EventCtrl.attendStatusOptions = attendStatusOptions;
+
+        EventCtrl.attend = {};
+
+        EventCtrl.searchValue = "";
+        EventCtrl.searchEvent = () => {
+            var result = {};
+            angular.forEach(EventCtrl.events, (value, key) => {
+                if (value.includes(EventCtrl.searchValue) || EventCtrl.searchValue == "") {
+                    result[key] = value;
+                }
+            });
+            return result;
+        };
+
+        EventCtrl.isAttending = (key) => {
+            return EventCtrl.attend[key] != undefined;
+        };
+
+        EventCtrl.attendEvent = (key) => {
+            EventCtrl.attend[key] = key;
+            console.log(EventCtrl.attendDetailInvited[key]);
+        };
+        EventCtrl.unattendEvent = (key) => {
+            delete EventCtrl.attend[key];
+            delete EventCtrl.attendDetailInterest[key];
+            delete EventCtrl.attendDetailStatus[key];
+        };
+
+
+        EventCtrl.submit = () => {
             EventCtrl.loader = true;
             EventCtrl.disableButton = true;
 
             console.log(EventCtrl.attend);
-            console.log(EventCtrl.interest);
         };
     };
 }
