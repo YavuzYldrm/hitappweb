81657d539b7da0d2b0e17508383b8d078acb97a7
Berkay Gürsoy <berkaygursoy@gmail.com>
2019-06-21 09:59:10 +0300

    heroku

--- public/services/Event/EventPredictService.js
+++ public/services/Event/EventPredictService.js
@@ -7,13 +7,16 @@ export default (app) => {
     function EventPredictService($http) {
         const PredictSrvc = this;
 
-        PredictSrvc.baseUrl = 'http://localhost:5000/api';
+        PredictSrvc.baseUrl = 'http://127.0.0.1:5000/api/v1';
 
-        PredictSrvc.getResult = function (item, response) {
+        PredictSrvc.getResult = function (features,model, response) {
             return $http({
                 method: 'POST',
                 url: PredictSrvc.baseUrl + '/predict',
-                data: item
+                data: {
+                    features:features,
+                    model:"event"
+                }
             });
         };
     };
