a14030e6b145dda36f89df0d323b9d9e72f0c3f8
Berkay Gürsoy <berkaygursoy@gmail.com>
2019-06-21 14:53:33 +0300

    localhost url

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -92,7 +92,7 @@ app.run(function ($rootScope, $window, $http, $cookies) {
         else {
             $http.defaults.headers.common['Authorization'] = 'Bearer ' + token;
 
-            const url = 'https://ml-tbt-hitappengine-api.herokuapp.com/api/v1/authenticate';
+            const url = 'http://localhost:5000/api/v1/authenticate';
             $http({
                 method: 'GET',
                 url: url
