4c872ae36ea71843dbd7f24398eddf93685f6eef
Berkay Gürsoy <berkaygursoy@gmail.com>
2019-06-25 14:28:07 +0300

    Add unit tests

---
+++ test/test_api.js
@@ -0,0 +1,22 @@
+//During the test the env variable is set to test
+process.env.NODE_ENV = 'test';
+
+//Require the dev-dependencies
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let server = require('../app');
+let should = chai.should();
+
+chai.use(chaiHttp);
+
+describe('/GET index', () => {
+    it('it should return content', (done) => {
+        chai.request(server)
+            .get('/')
+            .end((err, res) => {
+                res.should.have.status(200);
+                console.log(res);
+                done();
+            });
+    });
+});
