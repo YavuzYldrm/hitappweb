ea7f6f186dac6282254dd90442e1a908b67cc371
Berkay Gürsoy <berkaygursoy@gmail.com>
2019-06-25 13:12:52 +0300

    entry point as variable

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,11 @@ var express = require('express');
 var router = express.Router();
 var path = require('path');
 
+const entry = path.resolve('./public/recommendation/index.html');
+
 /* GET home page. */
-router.get('/*', function(req, res, next) {
-    res.sendFile(path.join(__dirname + '/index.html'));
+router.get('/', function(req, res, next) {
+    res.sendFile(entry);
 });
 
 module.exports = router;
